5bec4a234611d031a4a48fe36a802f3ff43d7140
Randy Russell <randyhr2000@gmail.com>
2022-12-21 01:23:37 -0800

    Game board working

--- src/flipper.js
+++ src/flipper.js
@@ -1,9 +1,7 @@
 FLIPPER_LENGTH = 70;
-FLIPPER_WIDTH = 10;
+FLIPPER_WIDTH = 20;
 
 function constructFlipper(gridPos, reverse) {
-    console.log(calculateWorldPos(gridPos));
-    console.log({x: (reverse? 1 : -1) * (FLIPPER_LENGTH/2 - 8), y:0});
     let flipperBody = Matter.Bodies.rectangle(0, 0, FLIPPER_LENGTH, FLIPPER_WIDTH)
     let flipperPivot = Matter.Constraint.create({
         bodyA: flipperBody,
